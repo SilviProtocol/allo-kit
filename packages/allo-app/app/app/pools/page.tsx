19691657ed027e7e42047caed0656855807a4c35
Carl Barrdahl <carlbarrdahl@gmail.com>
2025-06-04 15:18:31 +0200

    Add PoolList

--- packages/allo-app/app/app/pools/page.tsx
+++ packages/allo-app/app/app/pools/page.tsx
@@ -6,7 +6,9 @@ import { PoolList } from "~/components/pool/pool-list";
 export default function PoolsPage() {
   return (
     <Page title="Explore Pools" description="Browse and discover funding pools">
-      <div className="w-full flex-1">{/* <PoolList query={{}} /> */}</div>
+      <div className="w-full flex-1">
+        <PoolList query={{}} />
+      </div>
     </Page>
   );
 }

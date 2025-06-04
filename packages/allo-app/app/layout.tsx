1ad616e806d047776672c7e2703002e5b015047d
Carl Barrdahl <carlbarrdahl@gmail.com>
2025-06-04 10:28:16 +0200

    Build debug

--- packages/allo-app/app/layout.tsx
+++ packages/allo-app/app/layout.tsx
@@ -4,7 +4,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 import { Toaster } from "~/components/ui/sonner";
-import { Footer } from "./app/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",

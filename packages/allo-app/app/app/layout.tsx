1c7a165025cc67d90ed879dcef6dadb1cea13d27
Carl Barrdahl <carlbarrdahl@gmail.com>
2025-06-04 10:19:03 +0200

    Fix build

--- packages/allo-app/app/app/layout.tsx
+++ packages/allo-app/app/app/layout.tsx
@@ -4,7 +4,7 @@ import { Header } from "./header";
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <div className="max-w-screen-xl mx-auto flex flex-1 flex-col gap-2">
         <div className="p-2">{children}</div>
       </div>

b08eba78706c21d4a34ed09e5e7b80b19eadd22a
Carl Barrdahl <carlbarrdahl@gmail.com>
2025-06-09 23:51:43 +0200

    Update start block

--- packages/allo-indexer/ponder.config.ts
+++ packages/allo-indexer/ponder.config.ts
@@ -6,7 +6,7 @@ import { baseSepolia, hardhat } from "viem/chains";
 const isDev = process.env.NODE_ENV === "development";
 
 const targetNetwork = isDev ? hardhat : baseSepolia;
-const START_BLOCK = isDev ? 0 : 19501634;
+const START_BLOCK = isDev ? 0 : 26638593;
 
 const networks = {
   [targetNetwork.name]: {

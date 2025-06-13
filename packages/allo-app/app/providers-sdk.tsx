d2ca01b8a6e4febc0e9f3bcc14ceb658c18d9c12
Carl Barrdahl <carlbarrdahl@gmail.com>
2025-06-13 07:59:38 +0200

    Add AlloKitSDKProvider

---
+++ packages/allo-app/app/providers-sdk.tsx
@@ -0,0 +1,27 @@
+import { AlloKitSDK } from "@se-2/sdk";
+import { createContext, PropsWithChildren, useMemo } from "react";
+import { useCurrentChainName } from "~/hooks/use-current-chain";
+
+import * as chains from "viem/chains";
+import { useWalletClient } from "wagmi";
+
+export const AlloKitSDKContext = createContext<AlloKitSDK | null>(null);
+
+export function AlloKitSDKProvider({ children }: PropsWithChildren) {
+  const sdk = useAlloKitSDK();
+  return (
+    <AlloKitSDKContext.Provider value={sdk}>
+      {children}
+    </AlloKitSDKContext.Provider>
+  );
+}
+
+export function useAlloKitSDK() {
+  const network = useCurrentChainName();
+  const chainId = chains[network as keyof typeof chains]?.id;
+  const { data } = useWalletClient({ chainId });
+  return useMemo(
+    () => (data && chainId ? new AlloKitSDK(data, chainId) : null),
+    [data, chainId]
+  );
+}
